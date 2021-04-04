4855fff463d4f2c8ae8a923765c00b98243408a1
Duy Phạm Hoàng <phdu@netcompany.com>
2021-04-04 16:06:20 +0700

    Commit Lesson 7 - Exception

--- ConsoleAppLessons/ConsoleAppLesson7/Program.cs
+++ ConsoleAppLessons/ConsoleAppLesson7/Program.cs
@@ -7,7 +7,23 @@ namespace ConsoleAppLesson7
         static void Main(string[] args)
         {
             // Exception
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Result: " + num1 / num2);
 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+
+            Console.ReadLine();
         }
+
     }
 }
