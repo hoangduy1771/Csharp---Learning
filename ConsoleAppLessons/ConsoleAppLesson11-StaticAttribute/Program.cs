7c6782147bc17ce62611e6fbe719495f3948cf5a
Duy Phạm Hoàng <phdu@netcompany.com>
2021-04-07 00:13:33 +0700

    Commit Lesson 11 - Static attribute

---
+++ ConsoleAppLessons/ConsoleAppLesson11-StaticAttribute/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppLesson11_StaticAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song1 = new Song("21 Guns", "Green Day", 200);
+            Song song2 = new Song("Only When It Rains", "Astrid S", 250);
+
+            Console.WriteLine(song1.title);
+            Console.WriteLine(song2.title);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppLessons/ConsoleAppLesson11-StaticAttribute/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson11_StaticAttribute
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public Song(string sTitle, string sArtist, int sDuration)
+        {
+            title = sTitle;
+            artist = sArtist;
+            duration = sDuration;
+        }
+
+    }
+}
